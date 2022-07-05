a3191a0a8bb5932ad676c4af50806c071135f73f
uixcoder <idanylyuk@gmail.com>
2022-07-06 01:26:26 +0300

    fix files

--- Deploy/Terraform/AWS_Infrastructure.tf
+++ Deploy/Terraform/AWS_Infrastructure.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "App_PetClinic_TF" {
   }
 }
 resource "aws_instance" "DB_PetClinic_TF" {
-  ami                    = "ami-04e8b0e36ed3403dc"
+  ami                    = "ami-0917076ab9780844d"
   instance_type          = "t3.micro"
   key_name               = "ATC"
   vpc_security_group_ids = [aws_security_group.sg_db.id]
