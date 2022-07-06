7d9f4d6c55746d81023c870c1a56140eaa386146
uixcoder <idanylyuk@gmail.com>
2022-07-06 10:04:24 +0300

    fix files

--- Deploy/Terraform/AWS_Infrastructure.tf
+++ Deploy/Terraform/AWS_Infrastructure.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "pet-clinic-project"
-    key            = "pet-clinic-tf.tfstate"
-    encrypt        = true
-    region         = "eu-north-1"
+    bucket  = "pet-clinic-project"
+    key     = "pet-clinic-tf.tfstate"
+    encrypt = true
+    #region         = "eu-north-1"
     dynamodb_table = "petclinic-tf-lock"
   }
 
